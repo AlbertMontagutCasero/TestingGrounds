948361b3b7d88fb5233f4357773fe77f89ec60c5
amcgamer <albert.montagut.casero@gmail.com>
2017-11-30 18:52:26 +0100

    Initial commit

---
+++ Source/S05__TestingGrounds/S05__TestingGrounds.cpp
@@ -0,0 +1,7 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#include "S05__TestingGrounds.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, S05__TestingGrounds, "S05__TestingGrounds" );
+ 
---
+++ Source/S05__TestingGrounds/S05__TestingGroundsGameMode.cpp
@@ -0,0 +1,17 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#include "S05__TestingGroundsGameMode.h"
+#include "S05__TestingGroundsHUD.h"
+#include "S05__TestingGroundsCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AS05__TestingGroundsGameMode::AS05__TestingGroundsGameMode()
+	: Super()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	DefaultPawnClass = PlayerPawnClassFinder.Class;
+
+	// use our custom HUD class
+	HUDClass = AS05__TestingGroundsHUD::StaticClass();
+}
