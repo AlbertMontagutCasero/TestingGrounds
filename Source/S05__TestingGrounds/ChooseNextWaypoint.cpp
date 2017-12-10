6c946aeef237e00d775ca40685d28c9f6b31d8c3
Raikish <albert.montagut.casero@gmail.com>
2017-12-10 17:03:35 +0100

    creating the behavior tree on a c++

---
+++ Source/S05__TestingGrounds/ChooseNextWaypoint.cpp
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ChooseNextWaypoint.h"
+#include "AIController.h"
+#include "PatrolRoute.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+
+EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	
+
+
+	//Get the patrol Route
+	auto ControlledPawn = OwnerComp.GetAIOwner()->GetPawn();
+	auto PatrolRoute = ControlledPawn->FindComponentByClass<UPatrolRoute>();
+	if (!ensure(PatrolRoute)) { return EBTNodeResult::Failed; }
+	
+	auto PatrolPoints = PatrolRoute->GetPatrolPoints();
+	if (PatrolPoints.Num() == 0) 
+	{ 
+		auto GuardName = ControlledPawn->GetName();
+		UE_LOG(LogTemp, Warning, TEXT("A %s is missing the patrol points"));
+		return EBTNodeResult::Failed;
+	}
+
+
+	//Set next Waypoint
+	auto BlackBoardComp = OwnerComp.GetBlackboardComponent();
+	auto Index = BlackBoardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+	BlackBoardComp->SetValueAsObject(WaypointKey.SelectedKeyName, PatrolPoints[Index]);
+
+	//Cycle the index
+	auto NextIndex = (Index + 1) % PatrolPoints.Num();
+	BlackBoardComp->SetValueAsInt(IndexKey.SelectedKeyName, NextIndex);
+
+	return EBTNodeResult::Succeeded;
+}
