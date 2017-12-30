3890db9e50cbe3fe9cb97ec127811813ea5b3820
Raikish <albert.montagut.casero@gmail.com>
2017-12-30 18:00:38 +0100

    REFACTOR: Combine Third person and first person

--- Source/S05__TestingGrounds/S05__TestingGroundsGameMode.cpp
+++ Source/S05__TestingGrounds/S05__TestingGroundsGameMode.cpp
@@ -9,9 +9,9 @@ AS05__TestingGroundsGameMode::AS05__TestingGroundsGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Dynamic/Player/Behavior/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Dynamic/Character/Behaviour/Character"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
-
+	
 	// use our custom HUD class
 	HUDClass = AS05__TestingGroundsHUD::StaticClass();
 }
