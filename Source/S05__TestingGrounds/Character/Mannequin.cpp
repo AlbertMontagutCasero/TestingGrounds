82660c0e77e68dce2c013c68247c1d7957121c69
Raikish <albert.montagut.casero@gmail.com>
2018-01-04 16:45:37 +0100

    who pulled the trigger

--- Source/S05__TestingGrounds/Character/Mannequin.cpp
+++ Source/S05__TestingGrounds/Character/Mannequin.cpp
@@ -3,19 +3,47 @@
 #include "Mannequin.h"
 
 
+
 // Sets default values
 AMannequin::AMannequin()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = true; //TODO Check if it's needed
 
+	CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("FPCamera"));
+	CameraComponent->SetupAttachment(GetCapsuleComponent());
+	CameraComponent->RelativeLocation = FVector(-39.56f, 1.75f, 64.f); // Position the camera
+	CameraComponent->bUsePawnControlRotation = true;
+
+	// Create a mesh component that will be used when being viewed from a '1st person' view (when controlling this pawn)
+	Arms = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("FPCharacterArms"));
+	Arms->SetOnlyOwnerSee(true);
+	Arms->SetupAttachment(CameraComponent);
+	Arms->bCastDynamicShadow = false;
+	Arms->CastShadow = false;
+	Arms->RelativeRotation = FRotator(1.9f, -19.19f, 20.f);
+	Arms->RelativeLocation = FVector(-0.5f, -4.4f, -155.7f);
+	
 }
+
+
 
 // Called when the game starts or when spawned
 void AMannequin::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	if (BPGun == NULL)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Gun Blueprint is missing"));
+	}
+	Gun = GetWorld()->SpawnActor<AGun>(BPGun );
+	Gun->AttachToComponent(Arms, FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), TEXT("GripPoint"));
+	Gun->AnimInstance = Arms->GetAnimInstance();
+
+	if(InputComponent != NULL){
+		InputComponent->BindAction("Fire", IE_Pressed, this, &AMannequin::PullTrigger);
+	}
 }
 
 // Called every frame
@@ -30,5 +58,12 @@ void AMannequin::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	
+
+}
+
+void AMannequin::PullTrigger()
+{
+	Gun->OnFire();
 }
 
--- Source/S05__TestingGrounds/Character/Mannequin.h
+++ Source/S05__TestingGrounds/Character/Mannequin.h
@@ -3,7 +3,13 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "../Weapons/Gun.h"
+#include "Camera/CameraComponent.h"
 #include "GameFramework/Character.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/InputComponent.h"
+#include "GameFramework/InputSettings.h"
+#include "Components/SkeletalMeshComponent.h"
 #include "Mannequin.generated.h"
 
 UCLASS()
@@ -15,17 +21,32 @@ public:
 	// Sets default values for this character's properties
 	AMannequin();
 
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
-	
+	UPROPERTY(EditDefaultsOnly, Category = "Setup")
+	TSubclassOf<class AGun> BPGun;
+
+	UFUNCTION(BluePrintCallable, Category = "Weapon")
+	void PullTrigger();
+
+private:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	/** First person camera */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class UCameraComponent* CameraComponent;
+
+	/** arms */
+	UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
+	class USkeletalMeshComponent* Arms;
+
+	/** First person camera */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	AGun* Gun;
 	
 };
